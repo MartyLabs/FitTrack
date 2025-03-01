d155688c1aeb4ee5f6f35a13fe5ec05f2e89d51d
arthur.mariet@epitech.eu <arthur.mariet@epitech.eu>
2025-03-01 23:38:31 +0100

    chore: remove unnecessary setupFiles configuration in Jest

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   testEnvironment: "node",
-  setupFiles: ["<rootDir>/jest.setup.js"],
   collectCoverage: true,
   coverageDirectory: "coverage",
   testPathIgnorePatterns: ["/node_modules/"],
